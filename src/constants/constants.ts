6cad93834650750487a67498d0e29ed4eaa65007
dexterslabor <dexterslabor@gmail.com>
2021-01-04 18:30:50 +0000

    refactor(constants): remove runtime check, change how constants are handled

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,19 +1,8 @@
 import BigNumber from 'bignumber.js';
 
-import deploymentsJson from '../config/deployments.json';
-import config from '../config/sdkSettings.json';
-import { WrappedXTZBalance } from '../lib/types';
-
-import { Deployments, DeplyomentsRunType, SdkSettingsRunType } from './runTypes';
-
-// do guard checks
-if (!SdkSettingsRunType.guard(config)) {
-  throw new Error('sdk settings file is corrupted');
-}
-
-if (!DeplyomentsRunType.guard(deploymentsJson)) {
-  throw new Error('deployments properties file is corrupted');
-}
+import deploymentsJson from '../defaultConfig/deployments.json';
+import config from '../defaultConfig/sdkSettings.json';
+import { Deployments, WrappedXTZBalance } from '../lib/types';
 
 const defaultBalance: WrappedXTZBalance = new BigNumber(0);
 
